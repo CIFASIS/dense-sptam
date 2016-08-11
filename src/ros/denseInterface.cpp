11a73db01faeef7210da0d57b744841d8382144d
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2016-08-11 11:55:20 -0300

    ros: denseInterface: Add keyframe images subscriber
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/ros/denseInterface.cpp
+++ src/ros/denseInterface.cpp
@@ -12,7 +12,26 @@ namespace std
 
 dense::denseInterface::denseInterface(ros::NodeHandle& nh, ros::NodeHandle& nhp)
 {
+    /* Parameters */
+    bool use_approx_sync;
+    nhp.param<bool>("approximate_sync", use_approx_sync, false);
+
+    /* In/out topics */
     sub_path_ = nhp.subscribe("keyframes", 1, &denseInterface::cb_keyframes_path, this);
+
+    sub_img_l_.subscribe(nhp, "/keyframe/left/image_rect", 1);
+    sub_info_l_.subscribe(nhp, "/keyframe/left/camera_info", 1);
+    sub_img_r_.subscribe(nhp, "/keyframe/right/image_rect", 1);
+    sub_info_r_.subscribe(nhp, "/keyframe/right/camera_info", 1);
+
+    if (use_approx_sync) {
+        approximate_sync_.reset(new ApproximateSync(ApproximatePolicy(10),
+                                                    sub_img_l_, sub_info_l_, sub_img_r_, sub_info_r_));
+        approximate_sync_->registerCallback(boost::bind(&denseInterface::cb_images, this, _1, _2, _3, _4));
+    } else {
+        exact_sync_.reset(new ExactSync(ExactPolicy(1), sub_img_l_, sub_info_l_, sub_img_r_, sub_info_r_));
+        exact_sync_->registerCallback(boost::bind(&denseInterface::cb_images, this, _1, _2, _3, _4));
+    }
 
     ROS_INFO("DENSE node initialized.");
 }
@@ -28,4 +47,11 @@ dense::denseInterface::~denseInterface()
 void dense::denseInterface::cb_keyframes_path(const nav_msgs::PathConstPtr& path)
 {
     ROS_INFO("Keyframe path received.");
+}
+
+void dense::denseInterface::cb_images(
+    const sensor_msgs::ImageConstPtr& img_msg_left, const sensor_msgs::CameraInfoConstPtr& left_info,
+    const sensor_msgs::ImageConstPtr& img_msg_right, const sensor_msgs::CameraInfoConstPtr& right_info
+) {
+    ROS_INFO("Images received.");
 }
--- src/ros/denseInterface.hpp
+++ src/ros/denseInterface.hpp
@@ -1,9 +1,15 @@
 #ifndef DENSEINTERFACE_H
 #define DENSEINTERFACE_H
 
-#include <ros/ros.h>
 #include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
 #include <nav_msgs/Path.h>
+#include <ros/ros.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+#include <tf2_ros/transform_listener.h>
 
 namespace dense
 {
@@ -16,8 +22,31 @@ namespace dense
 
     private:
         void cb_keyframes_path(const nav_msgs::PathConstPtr& path);
+        void cb_images(const sensor_msgs::ImageConstPtr& img_msg_left, const sensor_msgs::CameraInfoConstPtr& left_info,
+                       const sensor_msgs::ImageConstPtr& img_msg_right, const sensor_msgs::CameraInfoConstPtr& right_info);
 
+        /* In/out topics */
         ros::Subscriber sub_path_;
+        message_filters::Subscriber<sensor_msgs::Image> sub_img_l_, sub_img_r_;
+        message_filters::Subscriber<sensor_msgs::CameraInfo> sub_info_l_, sub_info_r_;
+
+        /*
+         * From S-PTAM: Syncronizer for image messages. We don't know a priory which one
+         * will be used, so we have to define both, no common interface :/
+         */
+
+        /* Exact time image topic synchronizer */
+        typedef message_filters::sync_policies::ExactTime
+            <sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ExactPolicy;
+        typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+        boost::shared_ptr<ExactSync> exact_sync_;
+
+        /* Approximate time image topic synchronizer */
+        typedef message_filters::sync_policies::ApproximateTime
+            <sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ApproximatePolicy;
+        typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+        boost::shared_ptr<ApproximateSync> approximate_sync_;
+
     };
 
 }
