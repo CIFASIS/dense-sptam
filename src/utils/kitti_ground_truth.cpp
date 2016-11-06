02927a1880732210dc9b707b6ba8e77c9ae6737d
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2016-11-06 17:05:59 -0300

    dense: add kitti_ground_truth utils
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

---
+++ src/utils/kitti_ground_truth.cpp
@@ -0,0 +1,145 @@
+#include "kitti_ground_truth.hpp"
+#include "../dense/Camera.hpp"
+
+using namespace std;
+
+pair<Eigen::Matrix3d, Eigen::Vector3d> load_calib(const char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    pair<Eigen::Matrix3d, Eigen::Vector3d> pose;
+    char name[5];
+    int ret;
+
+    if (!fp)
+        exit(1);
+
+    while (!feof(fp)) {
+        Eigen::Matrix3d orientation;
+        Eigen::Vector3d position;
+
+        ret = fscanf(fp, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", name,
+                    &orientation.row(0)(0), &orientation.row(0)(1), &orientation.row(0)(2), &position(0),
+                    &orientation.row(1)(0), &orientation.row(1)(1), &orientation.row(1)(2), &position(1),
+                    &orientation.row(2)(0), &orientation.row(2)(1), &orientation.row(2)(2), &position(2));
+        if (ret == 13 && (strcmp(name, "Tr:") == 0)) {
+            pose = make_pair(orientation, position);
+            break;
+        }
+    }
+    if (feof(fp)) {
+        exit(1);
+    }
+    fclose(fp);
+    return pose;
+}
+
+vector<pair<Eigen::Matrix3d, Eigen::Vector3d>> load_poses(const char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    vector<pair<Eigen::Matrix3d, Eigen::Vector3d>> poses;
+    int ret;
+
+    if (!fp)
+        exit(1);
+
+    while (!feof(fp)) {
+        Eigen::Matrix3d orientation;
+        Eigen::Vector3d position;
+
+        ret = fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
+                    &orientation.row(0)(0), &orientation.row(0)(1), &orientation.row(0)(2), &position(0),
+                    &orientation.row(1)(0), &orientation.row(1)(1), &orientation.row(1)(2), &position(1),
+                    &orientation.row(2)(0), &orientation.row(2)(1), &orientation.row(2)(2), &position(2));
+        if (ret == 12) {
+            poses.push_back(make_pair(orientation, position));
+        }
+    }
+    fclose(fp);
+
+    return poses;
+}
+
+PointCloudPtr load_velodyne(const char *filename, pair<Eigen::Matrix3d, Eigen::Vector3d> calib,
+                            Eigen::Matrix3d orientation, Eigen::Vector3d position)
+{
+    PointCloudPtr ret(new PointCloud);
+    Eigen::Matrix3d calib_orientation = calib.first;
+    Eigen::Vector3d calib_position = calib.second;
+
+    // allocate 4 MB buffer (only ~130*4*4 KB are needed)
+    int32_t num = 1000000;
+    float *data = (float*)malloc(num * sizeof(float));
+
+    // pointers
+    float *px = data + 0;
+    float *py = data + 1;
+    float *pz = data + 2;
+    float *pr = data + 3;
+
+    // load point cloud
+    FILE *stream;
+    stream = fopen(filename, "r");
+    num = fread(data, sizeof(float), num, stream) / 4;
+    for (int32_t i = 0; i < num; i++) {
+        Eigen::Vector3d pt3d;
+        pt3d(0) = *px;
+        pt3d(1) = *py;
+        pt3d(2) = *pz;
+        pt3d = orientation * (calib_orientation * pt3d + calib_position) + position;
+        Point pt;
+        pt.x = pt3d(0);
+        pt.y = pt3d(1);
+        pt.z = pt3d(2);
+        pt.rgb = *pr;
+        ret->push_back(pt);
+        px += 4;
+        py += 4;
+        pz += 4;
+        pr += 4;
+    }
+
+    fclose(stream);
+
+    return ret;
+}
+
+namespace fs = boost::filesystem;
+
+int main(int argc, char* argv[])
+{
+    unsigned int count = 0;
+    fs::path full_path(fs::initial_path<fs::path>());
+
+    if (argc < 5) {
+        std::cout << "\nusage: " << argv[0] <<
+                     " [in-calib] [in-poses] [in-velo] [out-cloud]" << std::endl;
+        return -1;
+    }
+
+    pair<Eigen::Matrix3d, Eigen::Vector3d> calib = load_calib(argv[1]);
+    vector<pair<Eigen::Matrix3d, Eigen::Vector3d>> poses = load_poses(argv[2]);
+    full_path = fs::system_complete(fs::path(argv[3]));
+
+    if (!fs::exists(full_path) || !fs::is_directory(full_path)) {
+        std::cout << "\nDir not found: " << full_path.filename().c_str() << std::endl;
+        return 1;
+    }
+
+    fs::directory_iterator end_iter;
+    for (fs::directory_iterator dir_itr(full_path); dir_itr != end_iter; ++dir_itr) {
+        if (fs::is_regular_file(dir_itr->status())) {
+            Eigen::Matrix3d orientation = poses.at(count).first;
+            Eigen::Vector3d position = poses.at(count).second;
+            PointCloudPtr cloud = load_velodyne(dir_itr->path().c_str(), calib, orientation, position);
+            string filename(argv[4]);
+            filename = filename + dir_itr->path().filename().c_str() + ".pcd";
+            pcl::io::savePCDFileBinary(filename, *cloud);
+            count++;
+            std::cout << "Processed: " << dir_itr->path() << "\n";
+            std::cout << "    saved in: " << filename.c_str() << "\n";
+        };
+    }
+
+    cout << "TOTAL: " << count << " clouds" << endl;
+    return 0;
+}
---
+++ src/utils/kitti_ground_truth.hpp
@@ -0,0 +1,16 @@
+#ifndef KITTI_GROUND_TRUTH_H
+#define KITTI_GROUND_TRUTH_H
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fstream>
+
+
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+
+#include "../dense/PointCloudQueue.hpp"
+
+#endif
