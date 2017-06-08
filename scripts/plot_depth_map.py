11fa0b132daf7b953ea182367c7d2f0c7dca50b6
taihu <pire@cifasis-conicet.gov.ar>
2017-06-08 15:11:41 -0300

    converting tabs by spaces

--- scripts/plot_depth_map.py
+++ scripts/plot_depth_map.py
@@ -12,101 +12,101 @@ import os
 import pickle
 
 def listToArray(data, height, width):
-	ret = np.zeros((height, width))
-	for i in range(0, height):
-		for j in range(0, width):
-			ret[i, j] = data[i * width + j]
-	return ret
+  ret = np.zeros((height, width))
+  for i in range(0, height):
+    for j in range(0, width):
+      ret[i, j] = data[i * width + j]
+  return ret
 
 def arrayToList(arr):
-	return reduce(operator.add, arr.tolist())
+  return reduce(operator.add, arr.tolist())
 
 def applyMask(l, mask):
-	assert(len(l) == len(mask))
-	l_mask = zip(l, mask)
-	return map(lambda (x, y): x if y else -1, l_mask)
+  assert(len(l) == len(mask))
+  l_mask = zip(l, mask)
+  return map(lambda (x, y): x if y else -1, l_mask)
 
 def filterList(l, limit):
-	return map(lambda x: x if x <= limit else -1, l)
+  return map(lambda x: x if x <= limit else -1, l)
 
 def countValid(l):
-	return len(filter(lambda x: x >= 0, l))
+  return len(filter(lambda x: x >= 0, l))
 
 def intersectMask(dmap_x, dmap_y):
-	assert(dmap_x.shape[0] == dmap_y.shape[0])
-	assert(dmap_x.shape[1] == dmap_y.shape[1])
-	ret = np.zeros(dmap_x.shape[0], dmap_x.shape[1])
-	for i in range(0, dmap_x.shape[0]):
-		for j in range(0, dmap_x.shape[1]):
-			if (dmap_x[i, j] >= 0 and dmap_y[i, j] >= 0):
-				ret[i, j] = 1
-	return ret
+  assert(dmap_x.shape[0] == dmap_y.shape[0])
+  assert(dmap_x.shape[1] == dmap_y.shape[1])
+  ret = np.zeros(dmap_x.shape[0], dmap_x.shape[1])
+  for i in range(0, dmap_x.shape[0]):
+    for j in range(0, dmap_x.shape[1]):
+      if (dmap_x[i, j] >= 0 and dmap_y[i, j] >= 0):
+        ret[i, j] = 1
+  return ret
 
 def absoluteDiffList(l_x, l_y):
-	assert(len(l_x) == len(l_y))
-	return map(lambda (a, b): abs(a - b) if a >= 0 and b >= 0 else -1, zip(l_x, l_y))
+  assert(len(l_x) == len(l_y))
+  return map(lambda (a, b): abs(a - b) if a >= 0 and b >= 0 else -1, zip(l_x, l_y))
 
 INVALID_DEPTH = -10;
 
 def absoluteDiffMap(dmap_x, dmap_y):
-	assert(dmap_x.shape[0] == dmap_y.shape[0])
-	assert(dmap_x.shape[1] == dmap_y.shape[1])
-	ret = np.full((dmap_x.shape[0], dmap_x.shape[1]), INVALID_DEPTH)
-	for i in range(0, dmap_x.shape[0]):
-		for j in range(0, dmap_x.shape[1]):
-			if (dmap_x[i, j] >= 0 and dmap_y[i, j] >= 0):
-				ret[i, j] = abs(dmap_x[i, j] - dmap_y[i, j])
-	return ret
+  assert(dmap_x.shape[0] == dmap_y.shape[0])
+  assert(dmap_x.shape[1] == dmap_y.shape[1])
+  ret = np.full((dmap_x.shape[0], dmap_x.shape[1]), INVALID_DEPTH)
+  for i in range(0, dmap_x.shape[0]):
+    for j in range(0, dmap_x.shape[1]):
+      if (dmap_x[i, j] >= 0 and dmap_y[i, j] >= 0):
+        ret[i, j] = abs(dmap_x[i, j] - dmap_y[i, j])
+  return ret
 
 def getRGBcolor01(val, high, hsv_s = 0.8, hsv_v = 0.8, factor = 1):
-	hsv = ((1 - float(val) / float(high)) * factor, hsv_s, hsv_v)
-	return colorsys.hsv_to_rgb(*hsv)
+  hsv = ((1 - float(val) / float(high)) * factor, hsv_s, hsv_v)
+  return colorsys.hsv_to_rgb(*hsv)
 
 def getRGBcolor(val, high, hsv_s = 0.8, hsv_v = 0.8, factor = 1):
-	return tuple(int(i * 255) for i in getRGBcolor01(val, high, hsv_s, hsv_v, factor))
+  return tuple(int(i * 255) for i in getRGBcolor01(val, high, hsv_s, hsv_v, factor))
 
 def genRGBcolors(N):
-	return map(lambda x: getRGBcolor(x, N), range(N))
+  return map(lambda x: getRGBcolor(x, N), range(N))
 
 def doColorArray(arr, limit = 0):
-	background = (127, 127, 127)
-	if (not limit):
-		limit = np.amax(arr)
-	img = np.full((arr.shape[0], arr.shape[1], 3), background, np.uint8)
-	for i in range(0, arr.shape[0]):
-		for j in range(0, arr.shape[1]):
-			if (arr[i, j] >= 0):
-				img[i, j] = getRGBcolor(arr[i, j], limit)
-	return img
+  background = (127, 127, 127)
+  if (not limit):
+    limit = np.amax(arr)
+  img = np.full((arr.shape[0], arr.shape[1], 3), background, np.uint8)
+  for i in range(0, arr.shape[0]):
+    for j in range(0, arr.shape[1]):
+      if (arr[i, j] >= 0):
+        img[i, j] = getRGBcolor(arr[i, j], limit)
+  return img
 
 def saveImage(filename, img):
-	cv2.imwrite(filename, img)
+  cv2.imwrite(filename, img)
 
 def plotSaveImage(filename, img):
-	imgplot = plt.imshow(img)
-	plt.colorbar()
-	plt.savefig(filename, dpi=300)
+  imgplot = plt.imshow(img)
+  plt.colorbar()
+  plt.savefig(filename, dpi=300)
 
 class DepthMap:
 
-	def __init__(self, filename):
-		self.filename = filename
-		dmapfile = open(self.filename, "r")
-		self.parseheader(dmapfile.readline())
-		self.parsebody(dmapfile.readline())
-		dmapfile.close()
+  def __init__(self, filename):
+    self.filename = filename
+    dmapfile = open(self.filename, "r")
+    self.parseheader(dmapfile.readline())
+    self.parsebody(dmapfile.readline())
+    dmapfile.close()
 
-	def parseheader(self, line):
-		data = line.split(",")
-		self.height = int(data[0])
-		self.width = int(data[1])
+  def parseheader(self, line):
+    data = line.split(",")
+    self.height = int(data[0])
+    self.width = int(data[1])
 
-	def parsebody(self, line):
-		self.body = map(float, line.split(",")[:self.height * self.width])
-		self.body = map(lambda x: INVALID_DEPTH if x < 0 else x, self.body)
+  def parsebody(self, line):
+    self.body = map(float, line.split(",")[:self.height * self.width])
+    self.body = map(lambda x: INVALID_DEPTH if x < 0 else x, self.body)
 
-	def getnparray(self):
-		return listToArray(self.body, self.height, self.width)
+  def getnparray(self):
+    return listToArray(self.body, self.height, self.width)
 
 parser = argparse.ArgumentParser(epilog='NOTE: Invalid (negative) depth values are mapped to ' + str(INVALID_DEPTH))
 parser.add_argument('dmap', help='dmap file')
@@ -120,10 +120,10 @@ dmap_orig = DepthMap(args.dmap)
 dmap_orig_arr = dmap_orig.getnparray()
 
 if args.compare:
-	assert(os.path.isfile(args.compare))
-	dmap_compare = DepthMap(args.compare)
-	dmap_compare_arr = dmap_compare.getnparray()
-	dmap_orig_arr = absoluteDiffMap(dmap_orig_arr, dmap_compare_arr)
+  assert(os.path.isfile(args.compare))
+  dmap_compare = DepthMap(args.compare)
+  dmap_compare_arr = dmap_compare.getnparray()
+  dmap_orig_arr = absoluteDiffMap(dmap_orig_arr, dmap_compare_arr)
 
 fig, ax = plt.subplots()
 ax.set_axis_off()
