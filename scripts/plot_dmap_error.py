f0e54dbb495c71ae1e03319b88c8c0b78ad162fb
taihu <pire@cifasis-conicet.gov.ar>
2017-06-28 10:02:47 -0300

    removing outliers

--- scripts/plot_dmap_error.py
+++ scripts/plot_dmap_error.py
@@ -9,156 +9,156 @@ import glob
 import matplotlib.cbook as cbook
 
 def utf8(data):
-	return unicode(data, 'utf-8')
+  return unicode(data, 'utf-8')
 
 def make_fig_1(args):
-	limit = int(dmu.MAXDIFF_FIRST_GRAPH / dmu.STEP_FIRST_GRAPH)
-	domain = map(lambda x: x * dmu.STEP_FIRST_GRAPH, range(0, limit))
+  limit = int(dmu.MAXDIFF_FIRST_GRAPH / dmu.STEP_FIRST_GRAPH)
+  domain = map(lambda x: x * dmu.STEP_FIRST_GRAPH, range(0, limit))
 
-	diff_list_file = "depth_info/diff_list.npy"
-	if args.diff_list:
-		diff_list_file = args.diff_list
+  diff_list_file = "depth_info/diff_list.npy"
+  if args.diff_list:
+    diff_list_file = args.diff_list
 
-	if not(os.path.isfile(diff_list_file)):
-		print "Error: file not found: ", diff_list_file
-		return
+  if not(os.path.isfile(diff_list_file)):
+    print "Error: file not found: ", diff_list_file
+    return
 
-	print "Using diff list file ", diff_list_file
+  print "Using diff list file ", diff_list_file
 
-	# read from npy
-	diff_list = np.load(diff_list_file)
+  # read from npy
+  diff_list = np.load(diff_list_file)
 
-	# Discard 0 values at the end
-	diff_list_max = 0
-	for i in range(0, len(diff_list)):
-		if diff_list[i] > 0:
-			diff_list_max = i + 1
-	diff_list = diff_list[0:diff_list_max]
+  # Discard 0 values at the end
+  diff_list_max = 0
+  for i in range(0, len(diff_list)):
+    if diff_list[i] > 0:
+      diff_list_max = i + 1
+  diff_list = diff_list[0:diff_list_max]
 
-	major_ticks = np.arange(0, float(diff_list_max) * dmu.STEP_FIRST_GRAPH, 1)
-	minor_ticks = np.arange(0, float(diff_list_max) * dmu.STEP_FIRST_GRAPH, dmu.STEP_FIRST_GRAPH)
+  major_ticks = np.arange(0, float(diff_list_max) * dmu.STEP_FIRST_GRAPH, 1)
+  minor_ticks = np.arange(0, float(diff_list_max) * dmu.STEP_FIRST_GRAPH, dmu.STEP_FIRST_GRAPH)
 
-	fig, ax = plt.subplots()
+  fig, ax = plt.subplots()
 
-	index = np.arange(len(diff_list))
-	bar_width = 0.10
-	opacity = 0.7
+  index = np.arange(len(diff_list))
+  bar_width = 0.10
+  opacity = 0.7
 
-	title_font = {
-		'fontname':'Ubuntu', 'size':'18', 'color':'black', 'weight':'normal',
-		'verticalalignment':'bottom'
-	}
-	axis_font = {'fontname':'Ubuntu', 'size':'18'}
+  title_font = {
+    'fontname':'Ubuntu', 'size':'18', 'color':'black', 'weight':'normal',
+    'verticalalignment':'bottom'
+  }
+  axis_font = {'fontname':'Ubuntu', 'size':'18'}
 
-	rects1 = ax.bar(index * dmu.STEP_FIRST_GRAPH, diff_list, width=bar_width,
-					 align='edge', log=True,
-	                 alpha=opacity, color='b', edgecolor='b')
+  rects1 = ax.bar(index * dmu.STEP_FIRST_GRAPH, diff_list, width=bar_width,
+           align='edge', log=True,
+                   alpha=opacity, color='b', edgecolor='b')
 
-	plt.xlabel('Error (metros)', **axis_font)
-	plt.ylabel(utf8('Cantidad de píxeles'), **axis_font)
-	plt.title(utf8('Error - mapas de profundidad (dense vs. GT) para ' + args.sequence_name),
-			  **title_font)
+  plt.xlabel('Error (metros)', **axis_font)
+  plt.ylabel(utf8('Cantidad de píxeles'), **axis_font)
+  plt.title(utf8('Error - mapas de profundidad (dense vs. GT) para ' + args.sequence_name),
+        **title_font)
 
-	ax.set_frame_on(False)
-	plt.tick_params(axis='both', which='major', labelsize=15)
+  ax.set_frame_on(False)
+  plt.tick_params(axis='both', which='major', labelsize=15)
 
-	ax.set_xticks(major_ticks)
-	ax.set_xticks(minor_ticks, minor=True)
-	ax.set_yticks([], minor=True)
+  ax.set_xticks(major_ticks)
+  ax.set_xticks(minor_ticks, minor=True)
+  ax.set_yticks([], minor=True)
 
-	ax.grid(which='both')
-	ax.grid(which='minor', alpha=0.7)
-	ax.grid(which='major', alpha=1, linewidth=2)
+  ax.grid(which='both')
+  ax.grid(which='minor', alpha=0.7)
+  ax.grid(which='major', alpha=1, linewidth=2)
 
-	plt.tight_layout()
-	plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.9)
+  plt.tight_layout()
+  plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.9)
 
 
-	plt.savefig(args.sequence_name+"1.png")
+  plt.savefig(args.sequence_name+"1.png")
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('sequence_name', help='dataset sequence name')
-	parser.add_argument('--diff_list', help='string - name of diff_list file (needed for Graph 1, output of depth_map.py)')
-	parser.add_argument('--graph_depths', help='directory - graph files (needed for Graphs 2-5, output of depth_map.py)')
-	args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('sequence_name', help='dataset sequence name')
+  parser.add_argument('--diff_list', help='string - name of diff_list file (needed for Graph 1, output of depth_map.py)')
+  parser.add_argument('--graph_depths', help='directory - graph files (needed for Graphs 2-5, output of depth_map.py)')
+  args = parser.parse_args()
 
-	make_fig_1(args)
+  make_fig_1(args)
 
-	# depth vs errors (amount, mean)
-	# load all graph_depth*.npy files
+  # depth vs errors (amount, mean)
+  # load all graph_depth*.npy files
 
-	graph_depth_dir = "./depth_info/"
-	if args.graph_depths:
-		graph_depth_dir = args.graph_depths
+  graph_depth_dir = "./depth_info/"
+  if args.graph_depths:
+    graph_depth_dir = args.graph_depths
 
-	print "Using graph depth dir: ", graph_depth_dir
-	npys = glob.glob(graph_depth_dir+'graph_depth*.npy')
+  print "Using graph depth dir: ", graph_depth_dir
+  npys = glob.glob(graph_depth_dir+'graph_depth*.npy')
 
-	if len(npys)<=0 :
-		print "No data to collect.."
-		return
+  if len(npys)<=0 :
+    print "No data to collect.."
+    return
 
-	bins = np.load(graph_depth_dir+'bins.npy')
-	print bins
-	graph_depth = [[] for i in range(len(bins))]
+  bins = np.load(graph_depth_dir+'bins.npy')
+  print bins
+  graph_depth = [[] for i in range(len(bins))]
 
-	fig, ax = plt.subplots(1,1)
-	bxpstats = list()
-	graphs = []
-	for npy in npys:
-		g = np.load(npy)[1]
-		if len(g) > 0:
-			graphs.append(g)
+  fig, ax = plt.subplots(1,1)
+  bxpstats = list()
+  graphs = []
+  for npy in npys:
+    g = np.load(npy)[1]
+    if len(g) > 0:
+      graphs.append(g)
 
-	means = np.zeros(len(bins))
-	medians = np.zeros(len(bins))
-	maxs = np.zeros(len(bins))
-	for i in range(len(bins)):
-		graph_depth = []
+  means = np.zeros(len(bins))
+  medians = np.zeros(len(bins))
+  maxs = np.zeros(len(bins))
+  for i in range(len(bins)):
+    graph_depth = []
 
-		for j in range(len(npys)):
-			if i < len(graphs[j]):
-				if len(graphs[j][i]) > 0:
-					graph_depth.extend(graphs[j][i])
+    for j in range(len(npys)):
+      if i < len(graphs[j]):
+        if len(graphs[j][i]) > 0:
+          graph_depth.extend(graphs[j][i])
 
-		if len(graph_depth) > 0:
-			means[i] = np.mean(graph_depth)
-			medians[i] = np.median(graph_depth)
-			maxs[i] = np.max(graph_depth)
-			bxpstats.extend(cbook.boxplot_stats(np.ravel(graph_depth)))
-		else:
-			bxpstats.extend(cbook.boxplot_stats(np.ravel([0])))
+    if len(graph_depth) > 0:
+      means[i] = np.mean(graph_depth)
+      medians[i] = np.median(graph_depth)
+      maxs[i] = np.max(graph_depth)
+      bxpstats.extend(cbook.boxplot_stats(np.ravel(graph_depth)))
+    else:
+      bxpstats.extend(cbook.boxplot_stats(np.ravel([0])))
 
-		print "ITEM : " , i, len(graph_depth)
+    print "ITEM : " , i, len(graph_depth)
 
-	ax.bxp(bxpstats, positions=bins)
-	plt.xlabel("Distance to the camera (depth, m)")
-	plt.ylabel("Error (m)")
-	plt.savefig(args.sequence_name+"2.png")
+  ax.bxp(bxpstats, positions=bins, showfliers=False)
+  plt.xlabel("Distance to the camera (depth, m)")
+  plt.ylabel("Error (m)")
+  plt.savefig(args.sequence_name+"2.png")
 
-	# mean
-	plt.figure(3)
-	plt.plot(bins, means)
-	plt.xlabel("Distance to the camera (depth, m)")
-	plt.ylabel("Mean error (m)")
-	plt.savefig(args.sequence_name+"3.png")
+  # mean
+  plt.figure(3)
+  plt.plot(bins, means)
+  plt.xlabel("Distance to the camera (depth, m)")
+  plt.ylabel("Mean error (m)")
+  plt.savefig(args.sequence_name+"3.png")
 
-	plt.figure(4)
-	plt.plot(bins, medians)
-	plt.xlabel("Distance to the camera (depth, m)")
-	plt.ylabel("Median error (m)")
-	plt.savefig(args.sequence_name+"4.png")
+  plt.figure(4)
+  plt.plot(bins, medians)
+  plt.xlabel("Distance to the camera (depth, m)")
+  plt.ylabel("Median error (m)")
+  plt.savefig(args.sequence_name+"4.png")
 
-	plt.figure(5)
-	plt.plot(bins, maxs)
-	plt.xlabel("Distance to the camera (depth, m)")
-	plt.ylabel("Max error (m)")
-	plt.savefig(args.sequence_name+"5.png")
+  plt.figure(5)
+  plt.plot(bins, maxs)
+  plt.xlabel("Distance to the camera (depth, m)")
+  plt.ylabel("Max error (m)")
+  plt.savefig(args.sequence_name+"5.png")
 
-	print ""
-	print "Saved " + args.sequence_name + "{1-5}.png files"
+  print ""
+  print "Saved " + args.sequence_name + "{1-5}.png files"
 
 
 if __name__ == "__main__":
-	main()
+  main()
