ba151511200ef2aba87174e82469543e4b0661f9
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-12 15:54:26 -0300

    src: ros: Replace spaces by tabs
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/ros/denseNodelet.cpp
+++ src/ros/denseNodelet.cpp
@@ -6,21 +6,17 @@
 namespace dense
 {
 
-    class denseNodelet : public nodelet::Nodelet
-    {
+	class denseNodelet : public nodelet::Nodelet
+	{
+	public:
+		void onInit() {
+			NODELET_DEBUG("Initializing DENSE nodelet...");
+			dense_interface_.reset(new dense::denseInterface(getNodeHandle(), getPrivateNodeHandle()));
+		}
 
-    public:
-
-    void onInit() {
-        NODELET_DEBUG("Initializing DENSE nodelet...");
-        dense_interface_.reset(new dense::denseInterface(getNodeHandle(), getPrivateNodeHandle()));
-    }
-
-    private:
-
-    std::unique_ptr<dense::denseInterface> dense_interface_;
-
-    };
+	private:
+		std::unique_ptr<dense::denseInterface> dense_interface_;
+	};
 }
 
 PLUGINLIB_EXPORT_CLASS(dense::denseNodelet, nodelet::Nodelet)
--- src/ros/dense_node.cpp
+++ src/ros/dense_node.cpp
@@ -3,16 +3,16 @@
 
 int main(int argc, char* argv[])
 {
-    ros::init(argc, argv, "dense");
+	ros::init(argc, argv, "dense");
 
-    nodelet::Loader nodelet;
-    nodelet::M_string remap(ros::names::getRemappings());
-    nodelet::V_string nargv;
-    std::string nodelet_name = ros::this_node::getName();
-    nodelet.load(nodelet_name, "dense/denseNodelet", remap, nargv);
+	nodelet::Loader nodelet;
+	nodelet::M_string remap(ros::names::getRemappings());
+	nodelet::V_string nargv;
+	std::string nodelet_name = ros::this_node::getName();
+	nodelet.load(nodelet_name, "dense/denseNodelet", remap, nargv);
 
-    ros::MultiThreadedSpinner spinner;
-    spinner.spin();
+	ros::MultiThreadedSpinner spinner;
+	spinner.spin();
 
-    return 0;
+	return 0;
 }
