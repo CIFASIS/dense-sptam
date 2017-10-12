bdebc95f4df5d7575040e037269ec215b944d13a
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-12 17:21:10 -0300

    src: ImageQueue: Replace spaces by tabs
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/dense/ImageQueue.cpp
+++ src/dense/ImageQueue.cpp
@@ -8,34 +8,34 @@ ImageQueue::~ImageQueue()
 
 int ImageQueue::push(ImagePairPtr imagepair)
 {
-    int ret = 0;
-    std::lock_guard<std::mutex> lock(image_queue_lock_);
+	int ret = 0;
+	std::lock_guard<std::mutex> lock(image_queue_lock_);
 
-    if (image_ != nullptr)
-        ret = -1;
+	if (image_ != nullptr)
+		ret = -1;
 
-    image_ = imagepair;
-    empty_queue_cv.notify_all();
+	image_ = imagepair;
+	empty_queue_cv.notify_all();
 
-    return ret;
+	return ret;
 }
 
 ImagePairPtr ImageQueue::pop()
 {
-    std::mutex m;
-    std::unique_lock<std::mutex> lock(m);
+	std::mutex m;
+	std::unique_lock<std::mutex> lock(m);
 
-    image_queue_lock_.lock();
+	image_queue_lock_.lock();
 
-    while (image_ == nullptr) {
-        image_queue_lock_.unlock();
-        empty_queue_cv.wait(lock);
-        image_queue_lock_.lock();
-    }
+	while (image_ == nullptr) {
+		image_queue_lock_.unlock();
+		empty_queue_cv.wait(lock);
+		image_queue_lock_.lock();
+	}
 
-    ImagePairPtr ret = image_;
-    image_ = nullptr;
-    image_queue_lock_.unlock();
+	ImagePairPtr ret = image_;
+	image_ = nullptr;
+	image_queue_lock_.unlock();
 
-    return ret;
+	return ret;
 }
