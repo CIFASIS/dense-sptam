a2f1433304fb8e3077e6224d76a6c47e727006d4
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-12 17:13:09 -0300

    src: DisparityCalcThread: Replace spaces by tabs
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/dense/DisparityCalcThread.cpp
+++ src/dense/DisparityCalcThread.cpp
@@ -17,11 +17,11 @@
 DisparityCalcThread::DisparityCalcThread(Dense *dense)
   : dense_(dense)
 {
-    ROS_INFO("Selected disparity method: %s", dense_->parameters.disp_calc_method.c_str());
+	ROS_INFO("Selected disparity method: %s", dense_->parameters.disp_calc_method.c_str());
 
-    if (dense_->parameters.disp_calc_method == DISP_METHOD_OPENCV)
+	if (dense_->parameters.disp_calc_method == DISP_METHOD_OPENCV)
 		disparityCalcThread_ = new std::thread(&DisparityCalcThread::computeCV, this);
-    else if (dense_->parameters.disp_calc_method == DISP_METHOD_LIBELAS)
+	else if (dense_->parameters.disp_calc_method == DISP_METHOD_LIBELAS)
 		disparityCalcThread_ = new std::thread(&DisparityCalcThread::computeELAS, this);
 	else
 		ROS_ERROR("ERROR: Disparity method NOT FOUND: %s", dense_->parameters.disp_calc_method.c_str());
@@ -34,124 +34,123 @@ DisparityCalcThread::~DisparityCalcThread()
 
 void DisparityCalcThread::computeCV()
 {
-    cv::Ptr<cv::StereoBM> stereo = cv::StereoBM::create();
-    ImagePairPtr raw_image_pair;
-    ImagePtr raw_left_image, raw_right_image;
-    cv::Mat image_left, image_right, dmat;
-    DispRawImagePtr disp_raw_img;
-    DispImagePtr disp_img;
+	cv::Ptr<cv::StereoBM> stereo = cv::StereoBM::create();
+	ImagePairPtr raw_image_pair;
+	ImagePtr raw_left_image, raw_right_image;
+	cv::Mat image_left, image_right, dmat;
+	DispRawImagePtr disp_raw_img;
+	DispImagePtr disp_img;
 
-    while(1) {
-        /* Calls to pop() are blocking */
-        raw_image_pair = dense_->raw_image_pairs_->pop();
+	while(1) {
+		/* Calls to pop() are blocking */
+		raw_image_pair = dense_->raw_image_pairs_->pop();
 
-        double start_t, end_t;
-        start_t = GetSeg();
+		double start_t, end_t;
+		start_t = GetSeg();
 
-        raw_left_image = raw_image_pair->first;
-        raw_right_image = raw_image_pair->second;
+		raw_left_image = raw_image_pair->first;
+		raw_right_image = raw_image_pair->second;
 
-        image_left = cv_bridge::toCvCopy(raw_left_image, sensor_msgs::image_encodings::MONO8)->image;
-        image_right = cv_bridge::toCvCopy(raw_right_image, sensor_msgs::image_encodings::MONO8)->image;
+		image_left = cv_bridge::toCvCopy(raw_left_image, sensor_msgs::image_encodings::MONO8)->image;
+		image_right = cv_bridge::toCvCopy(raw_right_image, sensor_msgs::image_encodings::MONO8)->image;
 
-        stereo->compute(image_left, image_right, dmat);
+		stereo->compute(image_left, image_right, dmat);
 
-        disp_img = boost::make_shared<DispImage>(dmat);
-        disp_raw_img = boost::make_shared<DispRawImage>(raw_left_image, disp_img);
+		disp_img = boost::make_shared<DispImage>(dmat);
+		disp_raw_img = boost::make_shared<DispRawImage>(raw_left_image, disp_img);
 
-        if (dense_->disp_images_->push(disp_raw_img) < 0)
-            ROS_INFO("##### WARNING: Keyframe %u omitted, projectionThread busy! #####", raw_left_image->header.seq);
+		if (dense_->disp_images_->push(disp_raw_img) < 0)
+			ROS_INFO("##### WARNING: Keyframe %u omitted, projectionThread busy! #####", raw_left_image->header.seq);
 
-        end_t = GetSeg();
+			end_t = GetSeg();
 
-        dense_->WriteToLog("disparity,%u,%f\n", raw_left_image->header.seq, end_t - start_t);
-        ROS_INFO("Disparity  seq = %u (%f secs)", raw_left_image->header.seq, end_t - start_t);
+			dense_->WriteToLog("disparity,%u,%f\n", raw_left_image->header.seq, end_t - start_t);
+			ROS_INFO("Disparity  seq = %u (%f secs)", raw_left_image->header.seq, end_t - start_t);
 
 #if 0 /* Save disparity images */
-        char filename[256];
-        sprintf(filename, "images/orig_%u.jpg", raw_left_image->header.seq);
-        cv::imwrite(filename, image_left);
+		char filename[256];
+		sprintf(filename, "images/orig_%u.jpg", raw_left_image->header.seq);
+		cv::imwrite(filename, image_left);
 #endif
-    }
+	}
 }
 
 void DisparityCalcThread::computeELAS()
 {
-    ImagePairPtr raw_image_pair;
-    ImagePtr raw_left_image, raw_right_image;
-    cv::Mat image_left, image_right;
-    DispRawImagePtr disp_raw_img;
-    DispImagePtr disp_img;
-    int32_t dims[3];
+	ImagePairPtr raw_image_pair;
+	ImagePtr raw_left_image, raw_right_image;
+	cv::Mat image_left, image_right;
+	DispRawImagePtr disp_raw_img;
+	DispImagePtr disp_img;
+	int32_t dims[3];
 
-    float *D1_data = (float*)malloc(dense_->left_info_->width * dense_->left_info_->height * sizeof(float));
-    float *D2_data = (float*)malloc(dense_->right_info_->width * dense_->right_info_->height * sizeof(float));
-    cv::Mat_<float> *dmat = new cv::Mat_<float>(dense_->left_info_->height, dense_->left_info_->width, D1_data);
+	float *D1_data = (float*)malloc(dense_->left_info_->width * dense_->left_info_->height * sizeof(float));
+	float *D2_data = (float*)malloc(dense_->right_info_->width * dense_->right_info_->height * sizeof(float));
+	cv::Mat_<float> *dmat = new cv::Mat_<float>(dense_->left_info_->height, dense_->left_info_->width, D1_data);
 
-    Elas::parameters param(Elas::ROBOTICS);
+	Elas::parameters param(Elas::ROBOTICS);
 
-    if (dense_->parameters.sigma)
-        param.sigma = dense_->parameters.sigma;
-    param.add_corners = dense_->parameters.add_corners;
-    if (dense_->parameters.libelas_ipol_gap)
-        param.ipol_gap_width = dense_->parameters.libelas_ipol_gap;
+	if (dense_->parameters.sigma)
+		param.sigma = dense_->parameters.sigma;
+	param.add_corners = dense_->parameters.add_corners;
+	if (dense_->parameters.libelas_ipol_gap)
+		param.ipol_gap_width = dense_->parameters.libelas_ipol_gap;
 
-    Elas *elas = new Elas(param);
-    dims[2] = dims[0] = dense_->left_info_->width;
-    dims[1] = dense_->left_info_->height;
+	Elas *elas = new Elas(param);
+	dims[2] = dims[0] = dense_->left_info_->width;
+	dims[1] = dense_->left_info_->height;
 
-    while(1) {
-        /* Calls to pop() are blocking */
-        raw_image_pair = dense_->raw_image_pairs_->pop();
+	while(1) {
+		/* Calls to pop() are blocking */
+		raw_image_pair = dense_->raw_image_pairs_->pop();
 
-        double start_t, end_t;
-        start_t = GetSeg();
+		double start_t, end_t;
+		start_t = GetSeg();
 
-        raw_left_image = raw_image_pair->first;
-        raw_right_image = raw_image_pair->second;
+		raw_left_image = raw_image_pair->first;
+		raw_right_image = raw_image_pair->second;
 
-        image_left = cv_bridge::toCvCopy(raw_left_image, sensor_msgs::image_encodings::MONO8)->image;
-        image_right = cv_bridge::toCvCopy(raw_right_image, sensor_msgs::image_encodings::MONO8)->image;
+		image_left = cv_bridge::toCvCopy(raw_left_image, sensor_msgs::image_encodings::MONO8)->image;
+		image_right = cv_bridge::toCvCopy(raw_right_image, sensor_msgs::image_encodings::MONO8)->image;
 
-        elas->process(image_left.data, image_right.data, D1_data, D2_data, dims);
+		elas->process(image_left.data, image_right.data, D1_data, D2_data, dims);
 
-        disp_img = boost::make_shared<DispImage>(dmat);
-        disp_raw_img = boost::make_shared<DispRawImage>(raw_left_image, disp_img);
-        if (dense_->disp_images_->push(disp_raw_img) < 0)
-            ROS_INFO("##### WARNING: Keyframe %u omitted, projectionThread busy! #####", raw_left_image->header.seq);
+		disp_img = boost::make_shared<DispImage>(dmat);
+		disp_raw_img = boost::make_shared<DispRawImage>(raw_left_image, disp_img);
+		if (dense_->disp_images_->push(disp_raw_img) < 0)
+			ROS_INFO("##### WARNING: Keyframe %u omitted, projectionThread busy! #####", raw_left_image->header.seq);
 
-        end_t = GetSeg();
+		end_t = GetSeg();
 
-        dense_->WriteToLog("disparity,%u,%f\n", raw_left_image->header.seq, end_t - start_t);
-        ROS_INFO("Disparity  seq = %u (%f secs)", raw_left_image->header.seq, end_t - start_t);
+		dense_->WriteToLog("disparity,%u,%f\n", raw_left_image->header.seq, end_t - start_t);
+		ROS_INFO("Disparity  seq = %u (%f secs)", raw_left_image->header.seq, end_t - start_t);
 
 #if 0 /* Save disparity image and depth map */
-        char filename[256];
-        sprintf(filename, "images/%06d.dmap", raw_left_image->header.seq);
-        // Alloc memory for depth_map
-        float *depth_data = (float*)malloc(dense_->left_info_->width * dense_->left_info_->height * sizeof(float));
+		char filename[256];
+		sprintf(filename, "images/%06d.dmap", raw_left_image->header.seq);
+		// Alloc memory for depth_map
+		float *depth_data = (float*)malloc(dense_->left_info_->width * dense_->left_info_->height * sizeof(float));
 
-        // convert disparity image to depth map
-        for ( size_t i = 0; i < (dense_->left_info_->height * dense_->left_info_->width); ++i) {
-          float disp = D1_data[i];
-          // check if disparity is invalid
-          if (!finite(disp) || disp <= 0) {
-            depth_data[i] = PIXEL_DEPTH_INVALID;
-          }
-          else {
-            // compute depth
-            float depth = dense_->camera_->getStereoModel().getZ( disp );
-            // check if depth is valid
-//            if (depth <= dense_->max_distance_) {
-              depth_data[i] = depth;
-//            }
-//            else {
-//              depth_data[i] = PIXEL_DEPTH_INVALID;
-//            }
-          }
-        }
+		// convert disparity image to depth map
+		for ( size_t i = 0; i < (dense_->left_info_->height * dense_->left_info_->width); ++i) {
+			float disp = D1_data[i];
+			// check if disparity is invalid
+			if (!finite(disp) || disp <= 0) {
+				depth_data[i] = PIXEL_DEPTH_INVALID;
+			} else {
+			// compute depth
+			float depth = dense_->camera_->getStereoModel().getZ( disp );
+			// check if depth is valid
+			// if (depth <= dense_->max_distance_) {
+					depth_data[i] = depth;
+			// }
+			// else {
+			//		depth_data[i] = PIXEL_DEPTH_INVALID;
+			// }
+			}
+		}
 
-        saveDepthImage(depth_data, dense_->left_info_->height, dense_->left_info_->width, filename);
+		saveDepthImage(depth_data, dense_->left_info_->height, dense_->left_info_->width, filename);
 #endif
-    }
+	}
 }
