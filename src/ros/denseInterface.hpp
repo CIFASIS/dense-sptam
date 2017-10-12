a8e01181da47f549b0d9382db84254cb760c77a6
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-12 15:15:01 -0300

    src: denseInterface: Replace spaces by tabs in header
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/ros/denseInterface.hpp
+++ src/ros/denseInterface.hpp
@@ -17,54 +17,53 @@
 
 namespace dense
 {
+	class denseInterface
+	{
+	public:
+		denseInterface(ros::NodeHandle& nh, ros::NodeHandle& nhp);
+		~denseInterface();
 
-    class denseInterface
-    {
-    public:
-        denseInterface(ros::NodeHandle& nh, ros::NodeHandle& nhp);
-        ~denseInterface();
+	private:
+		void cb_keyframes_path(const nav_msgs::PathConstPtr& path);
+		void cb_images(const sensor_msgs::ImageConstPtr& img_msg_left,
+					   const sensor_msgs::CameraInfoConstPtr& left_info,
+					   const sensor_msgs::ImageConstPtr& img_msg_right,
+					   const sensor_msgs::CameraInfoConstPtr& right_info);
 
-    private:
-
-        void cb_keyframes_path(const nav_msgs::PathConstPtr& path);
-        void cb_images(const sensor_msgs::ImageConstPtr& img_msg_left, const sensor_msgs::CameraInfoConstPtr& left_info,
-                       const sensor_msgs::ImageConstPtr& img_msg_right, const sensor_msgs::CameraInfoConstPtr& right_info);
-
-        /* Parameters */
 		struct dense_parameters parameters;
 
-        /* In/out topics */
-        ros::Subscriber sub_path_;
-        message_filters::Subscriber<sensor_msgs::Image> sub_img_l_, sub_img_r_;
-        message_filters::Subscriber<sensor_msgs::CameraInfo> sub_info_l_, sub_info_r_;
-        ros::Publisher pub_map_, pub_map_bad_;
+		/* In/out topics */
+		ros::Subscriber sub_path_;
+		message_filters::Subscriber<sensor_msgs::Image> sub_img_l_, sub_img_r_;
+		message_filters::Subscriber<sensor_msgs::CameraInfo> sub_info_l_, sub_info_r_;
+		ros::Publisher pub_map_, pub_map_bad_;
 
-        /*
-         * From S-PTAM: Syncronizer for image messages. We don't know a priory which one
-         * will be used, so we have to define both, no common interface :/
-         */
+		/*
+		 * From S-PTAM: Syncronizer for image messages. We don't know a priory which one
+		 * will be used, so we have to define both, no common interface :/
+		 */
 
-        /* Exact time image topic synchronizer */
-        typedef message_filters::sync_policies::ExactTime
-            <sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ExactPolicy;
-        typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
-        boost::shared_ptr<ExactSync> exact_sync_;
+		/* Exact time image topic synchronizer */
+		typedef message_filters::sync_policies::ExactTime
+			<sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ExactPolicy;
+		typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+		boost::shared_ptr<ExactSync> exact_sync_;
 
-        /* Approximate time image topic synchronizer */
-        typedef message_filters::sync_policies::ApproximateTime
-            <sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ApproximatePolicy;
-        typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
-        boost::shared_ptr<ApproximateSync> approximate_sync_;
+		/* Approximate time image topic synchronizer */
+		typedef message_filters::sync_policies::ApproximateTime
+			<sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::Image, sensor_msgs::CameraInfo> ApproximatePolicy;
+		typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
+		boost::shared_ptr<ApproximateSync> approximate_sync_;
 
-        /* Transform */
-        tf2_ros::Buffer tfBuffer_;
-        tf2_ros::TransformListener transform_listener_;
+		/* Transform */
+		tf2_ros::Buffer tfBuffer_;
+		tf2_ros::TransformListener transform_listener_;
 
-        /* DENSE */
-        Dense *dense_;
+		/* DENSE */
+		Dense *dense_;
 
-        uint32_t last_publish_seq_;
-    };
-
+		uint32_t last_publish_seq_;
+	};
 }
+
 #endif /* DENSEINTERFACE_H */
