54729038cdf4e52c18b15638689733be3a0f4123
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-12 16:17:58 -0300

    src: kitti_dmap: Replace spaces by tabs in header
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- src/utils/kitti_dmap.hpp
+++ src/utils/kitti_dmap.hpp
@@ -14,13 +14,13 @@
 #include "../dense/dense.hpp"
 
 int generate_depth_maps_kitti_global(const char *in_poses_path, const char *in_clouds_path,
-                                     const char *out_path, int region_size, double pub_area_filter_min,
-                                     Dense *dense_);
+									 const char *out_path, int region_size, double pub_area_filter_min,
+									 Dense *dense_);
 int generate_depth_maps_euroc_global(const char *in_poses_path, const char *in_timestamps_path,
-                                     const char *in_clouds_path, const char *out_path,
-                                     double pub_area_filter_min, Dense *dense_);
+									 const char *in_clouds_path, const char *out_path,
+									 double pub_area_filter_min, Dense *dense_);
 int generate_depth_maps_kitti_local(const char *in_poses_path, const char *in_clouds_path,
-                                    const char *out_path, double pub_area_filter_min, Dense *dense_);
+									const char *out_path, double pub_area_filter_min, Dense *dense_);
 
 void saveDepthImage(float *depth_map_data, int img_height, int img_width, const char *filename);
 
