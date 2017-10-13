71a20cdc3d7abf6fed8cfa5caf1327d242286a55
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-13 16:45:12 -0300

    Remove misc/ directory
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- misc/other.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./devel/lib/dense/kitti_ground_truth \
-	../../datasets/kitti/sequences/04/calib.txt \
-	../../datasets/kitti/poses/04.txt ../../datasets/kitti/sequences/04/velodyne/ \
-	../../datasets/kitti/sequences/04/velo_clouds/ \
-
