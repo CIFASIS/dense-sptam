4ef404e09d48981eac4ba91d9486611a6ae6e040
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-17 18:13:59 -0300

    scripts: Merge plot_cloud_size script into profiling
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- scripts/compute_and_plot.sh
+++ scripts/compute_and_plot.sh
@@ -2,26 +2,32 @@
 
 # Input parameters examples:
 #
-#   Kitti:
-#   dir_dense = "~/.ros/kitti04/dmap/"
-#   dir_gt = "~/datasets/kitti04GT/dmap/"
-#   sequence = "kitti04_"
+#  Kitti:
+#  dir_dense_log="~/.ros/kitti04/dense_node.log"
+#  dir_dense_pcd="~/.ros/kitti04/pcd/"
+#  dir_dense_dmap="~/.ros/kitti04/dmap/"
+#  dir_gt_dmap="~/datasets/kitti04GT/dmap/"
+#  sequence="kitti04_"
 #
-#   Tsukuba:
-#   dir_dense = "~/.ros/tsukuba/dmap/"
-#   dir_gt = "~/datasets/tsukuba_dataset/depth_maps/left/dmap/"
-#   sequence = "tsukuba"
+#  Tsukuba:
+#  dir_dense_log="~/.ros/tsukuba/dense_node.log"
+#  dir_dense_pcd="~/.ros/tsukuba/pcd/"
+#  dir_dense_dmap = "~/.ros/tsukuba/dmap/"
+#  dir_gt_dmap = "~/datasets/tsukuba_dataset/depth_maps/left/dmap/"
+#  sequence = "tsukuba"
 
-dir_dense="$1"
-dir_gt="$2"
-sequence="$3"
+dir_dense_log="$1"
+dir_dense_pcd="$2"
+dir_dense_dmap="$3"
+dir_gt_dmap="$4"
+sequence="$5"
 
 usage () {
-	echo "usage: $0 <dense-dmap-dir> <gt-dmap-dir> <sequence-name>" >&2
+	echo "usage: $0 <dense-log-file> <dense-pcd-dir> <dense-dmap-dir> <gt-dmap-dir> <sequence-name>" >&2
 	exit 1
 }
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 5 ]; then
 	usage
 fi
 
@@ -33,5 +39,15 @@ python depth_map.py $dir_dense $dir_gt --show_time 1 --min_dist 5.0
 
 echo "Plotting..."
 python plot_dmap_error.py $sequence
+
+echo "Profiling..."
+
+# Binaries/lib build path is assumed to be there
+profiling=$(../../../devel/lib/dense/point_cloud_profiling $dir_dense_pcd | tail -n3)
+total=$(echo $profiling | cut -d' ' -f3)
+hypothesis=$(echo $profiling | cut -d' ' -f6)
+validated=$(echo $profiling | cut -d' ' -f9)
+
+python profiling.py $dir_dense_log $sequence --hypothesis $hypothesis --validated $validated
 
 echo "End"
--- scripts/profiling.py
+++ scripts/profiling.py
@@ -86,19 +86,111 @@ def plot_phase_time(data):
 	render_img(plt, 'phase_time.png')
 
 
-TASK_DISPARITY = 0;
-TASK_STEREOSCAN = 1;
-TASK_PROJECTION = 2;
-TASK_REFINEMENT = 3;
+def plot_cloud_size(data):
 
-STEREOSCAN_NEW = 0;
-STEREOSCAN_MATCH = 1;
-STEREOSCAN_UNMATCH = 2;
-STEREOSCAN_OUTLIER = 3;
+	config = [
+		{
+			'field': 'created',
+			'label': 'Puntos creados',
+			'offset': 3.5,
+			'color': 'b',
+		},
+		{
+			'field': 'outliers',
+			'label': 'Puntos descartados',
+			'offset': 4.5,
+			'color': 'g',
+		},
+		{
+			'field': 'total',
+			'label': 'Puntos finales',
+			'offset': 5.5,
+			'color': 'r',
+		},
+		{
+			'field': 'hypothesis',
+			'label': 'Puntos finales (hipótesis)',
+			'offset': 6.7,
+			'color': 'c',
+		},
+		{
+			'field': 'validated',
+			'label': 'Puntos finales (validados)',
+			'offset': 7.7,
+			'color': 'm',
+		},
+		{
+			'field': 'matches',
+			'label': 'Cantidad de fusiones',
+			'offset': 8.9,
+			'color': 'y',
+		},
+	]
+
+	# Scale to millons of points
+	def apply_factor(l):
+		scale_factor = 1000000
+		return map(lambda x: x / scale_factor, l)
+
+	fig, ax = plt.subplots()
+
+	index = np.arange(len(data))
+	bar_width = 0.2
+	opacity = 0.9
+
+	for field_config in config:
+		field_name = field_config['field']
+		label = utf8(field_config['label'])
+		offset = field_config['offset']
+		color = field_config['color']
+
+		bar_data = [sequence_data[field_name] for sequence_name, sequence_data in data.items()]
+		bar_data = apply_factor(bar_data)
+
+		ax.bar(2 * index + offset * bar_width, bar_data, width=bar_width,
+			align='edge', alpha=opacity, color=color, label=label)
+
+	title_font = {
+		'fontname':'Ubuntu', 'size':'46', 'color':'black', 'weight':'normal',
+		'verticalalignment':'bottom'
+	}
+	axis_font = {'fontname':'Ubuntu', 'size':'46'}
+
+	plt.xlabel('', **axis_font)
+	plt.ylabel('Millones de puntos', **axis_font)
+	plt.title('Resultados para el dataset Tsukuba', **title_font)
+
+	ax.set_frame_on(False)
+	plt.tick_params(axis='both', which='major', labelsize=46)
+
+	plt.xticks(2 * index + 6 * bar_width, '', rotation=90, horizontalalignment='center')
+
+	ax.xaxis.grid(False)
+	ax.yaxis.grid(True, linewidth=5)
+	#ax.set_ylim([0,140])
+
+	plt.legend(loc='upper left', fontsize=40)
+
+	plt.tight_layout()
+	render_img(plt, 'cloud_size.png')
+
+
+TASK_DISPARITY = 0;
+TASK_PROJECTION = 1;
+TASK_REFINEMENT = 2;
+TASK_LEN = 3;
+
+POINT_TYPE_NEW = 0;
+POINT_TYPE_MATCH = 1;
+POINT_TYPE_UNMATCH = 2;
+POINT_TYPE_OUTLIER = 3;
+POINT_TYPE_LEN = 4;
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dense_log', help='dense node log')
 parser.add_argument('sequence_name', help='sequence name')
+parser.add_argument('--hypothesis', help='set hypothesis points number')
+parser.add_argument('--validated', help='set validated points number')
 parser.add_argument('--show', help='show images instead of saving', action="store_true")
 args = parser.parse_args()
 
@@ -107,9 +199,9 @@ assert(os.path.isfile(args.dense_log))
 # Output log
 logfile = open(args.dense_log, "r")
 
-task_count = [0] * 4
-task_time = [0.0] * 4
-stereoscan_points = [0] * 5
+task_count = [0] * TASK_LEN
+task_time = [0.0] * TASK_LEN
+points = [0] * POINT_TYPE_LEN
 
 for line in logfile.readlines():
 
@@ -124,48 +216,62 @@ for line in logfile.readlines():
   if task == 'disparity':
     task_count[TASK_DISPARITY] += 1
     task_time[TASK_DISPARITY] += time
-  elif task == 'stereoscan':
-    task_count[TASK_STEREOSCAN] += 1
-    task_time[TASK_STEREOSCAN] += time
-    stereoscan_points[STEREOSCAN_NEW] += int(data[3])
-    stereoscan_points[STEREOSCAN_MATCH] += int(data[4])
-    stereoscan_points[STEREOSCAN_UNMATCH] += int(data[5])
-    stereoscan_points[STEREOSCAN_OUTLIER] += int(data[6])
   elif task == 'projection':
     task_count[TASK_PROJECTION] += 1
     task_time[TASK_PROJECTION] += time
+    points[POINT_TYPE_NEW] += int(data[3])
+    points[POINT_TYPE_MATCH] += int(data[4])
+    points[POINT_TYPE_UNMATCH] += int(data[5])
+    points[POINT_TYPE_OUTLIER] += int(data[6])
   elif task == 'refinement':
     task_count[TASK_REFINEMENT] += 1
     task_time[TASK_REFINEMENT] += time
-
-assert(task_count[TASK_STEREOSCAN] == task_count[TASK_PROJECTION])
 
 task_time_mean = map(lambda (x, y): (y / x) * 1000, zip(task_count, task_time))
 
 print "Keyframes processed per phase"
 print "    Disparity:            " + str(task_count[TASK_DISPARITY])
-print "    Heuristic/fusion:     " + str(task_count[TASK_STEREOSCAN])
+print "    Heuristic/fusion:     " + str(task_count[TASK_PROJECTION])
 print "    Refinement:           " + str(task_count[TASK_REFINEMENT])
 print ""
 
 print "Mean time per phase (ms)"
 print "    Disparity:            " + str(task_time_mean[TASK_DISPARITY])
-print "    Heuristic/fusion:     " + str(task_time_mean[TASK_STEREOSCAN] + task_time_mean[TASK_PROJECTION])
+print "    Heuristic/fusion:     " + str(task_time_mean[TASK_PROJECTION])
 print "    Refinement:           " + str(task_time_mean[TASK_REFINEMENT])
 print ""
 
 print "Heuristic results (points)"
-print "    Total points created: " + str(stereoscan_points[STEREOSCAN_NEW])
-print "    Fusions/matches:      " + str(stereoscan_points[STEREOSCAN_MATCH])
-print "    Outliers:             " + str(stereoscan_points[STEREOSCAN_OUTLIER])
+print "    Total points created: " + str(points[POINT_TYPE_NEW])
+print "    Fusions/matches:      " + str(points[POINT_TYPE_MATCH])
+print "    Outliers:             " + str(points[POINT_TYPE_OUTLIER])
 print ""
 
 phase_time_data = {
 	args.sequence_name: {
 		'disparity': task_time_mean[TASK_DISPARITY],
-		'projection': task_time_mean[TASK_STEREOSCAN] + task_time_mean[TASK_PROJECTION],
+		'projection': task_time_mean[TASK_PROJECTION],
 		'refinement': task_time_mean[TASK_REFINEMENT],
 	}
 }
 
 plot_phase_time(phase_time_data)
+
+cloud_size_data = {
+	args.sequence_name: {
+		# Points that were created (triangulated) from a keyframe.
+		'created': points[POINT_TYPE_NEW],
+		# Points discarded as outliers.
+		'outliers': points[POINT_TYPE_OUTLIER],
+		# Final point cloud size (composed by hypothesis and validated points).
+		'total': int(args.hypothesis) + int(args.validated),
+		# Number of hypothesis in final point cloud.
+		'hypothesis': int(args.hypothesis),
+		# Number of hypothesis in final point cloud.
+		'validated': int(args.validated),
+		# Number of matches during the sequence, i.e. number of fusions.
+		'matches': points[POINT_TYPE_MATCH],
+	}
+}
+
+plot_cloud_size(cloud_size_data)
