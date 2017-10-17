fc40b9db0a653e65037a40771de04c5bab4337f6
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2017-10-17 11:42:42 -0300

    scripts: Merge plot_phase_time script into profiling
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- scripts/profiling.py
+++ scripts/profiling.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import argparse
 import colorsys
 import csv
@@ -11,6 +13,79 @@ import operator
 import os
 import pickle
 
+def utf8(data):
+	return unicode(data, 'utf-8')
+
+def render_img(plt, filename):
+	if (args.show):
+		plt.show()
+	else:
+		plt.savefig(filename)
+
+def plot_phase_time(data):
+
+	config = [
+		{
+			'field': 'disparity',
+			'label': 'Cómputo de disparidad',
+			'offset': 3.5,
+			'color': 'b',
+		},
+		{
+			'field': 'projection',
+			'label': 'Expansión y fusión de mapa',
+			'offset': 4.5,
+			'color': 'g',
+		},
+		{
+			'field': 'refinement',
+			'label': 'Refinamiento de mapa',
+			'offset': 5.5,
+			'color': 'r',
+		},
+	]
+
+	fig, ax = plt.subplots()
+
+	index = np.arange(len(data))
+	bar_width = 0.2
+	opacity = 0.9
+
+	for field_config in config:
+		field_name = field_config['field']
+		label = utf8(field_config['label'])
+		offset = field_config['offset']
+		color = field_config['color']
+
+		bar_data = [sequence_data[field_name] for sequence_name, sequence_data in data.items()]
+
+		ax.bar(2 * index + offset * bar_width, bar_data, width=bar_width,
+			align='edge', alpha=opacity, color=color, label=label)
+
+	title_font = {
+		'fontname':'Ubuntu', 'size':'24', 'color':'black', 'weight':'normal',
+		'verticalalignment':'bottom'
+	}
+	axis_font = {'fontname':'Ubuntu', 'size':'18'}
+
+	plt.xlabel('', **axis_font)
+	plt.ylabel('Tiempo (ms)', **axis_font)
+	plt.title('Tiempo promedio por etapa para el dataset Tsukuba', **title_font)
+
+	ax.set_frame_on(False)
+	plt.tick_params(axis='both', which='major', labelsize=15)
+
+	plt.xticks(2 * index + 6 * bar_width, '', rotation=90, horizontalalignment='center')
+
+	ax.xaxis.grid(False)
+	ax.yaxis.grid(True)
+
+	plt.legend()
+
+	plt.tight_layout()
+	render_img(plt, 'phase_time.png')
+
+
 TASK_DISPARITY = 0;
 TASK_STEREOSCAN = 1;
 TASK_PROJECTION = 2;
@@ -23,6 +98,8 @@ STEREOSCAN_OUTLIER = 3;
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dense_log', help='dense node log')
+parser.add_argument('sequence_name', help='sequence name')
+parser.add_argument('--show', help='show images instead of saving', action="store_true")
 args = parser.parse_args()
 
 assert(os.path.isfile(args.dense_log))
@@ -82,3 +159,13 @@ print "    Total points created: " + str(stereoscan_points[STEREOSCAN_NEW])
 print "    Fusions/matches:      " + str(stereoscan_points[STEREOSCAN_MATCH])
 print "    Outliers:             " + str(stereoscan_points[STEREOSCAN_OUTLIER])
 print ""
+
+phase_time_data = {
+	args.sequence_name: {
+		'disparity': task_time_mean[TASK_DISPARITY],
+		'projection': task_time_mean[TASK_STEREOSCAN] + task_time_mean[TASK_PROJECTION],
+		'refinement': task_time_mean[TASK_REFINEMENT],
+	}
+}
+
+plot_phase_time(phase_time_data)
